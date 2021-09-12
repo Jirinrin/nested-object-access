eb9b19ec452ab5ce6a68f5e93ad1e19785a5828e
Jiri Swen <jiri.ei@hotmail.com>
2021-09-12 13:21:54 +0200

    Improve test suite

--- src/__tests__/getNested.ts
+++ src/__tests__/getNested.ts
@@ -1,20 +1,30 @@
 import { getNested } from "..";
 
 const testObj = {
-  b: "bb",
-  c: { d: "dd", w: "ww", e: { p: "pp" } },
-  t: { l: "ww", e: { q: "pp" } },
-  i: { d: "DD", o: "oo" },
+  a: "a",
+  b: { c: "c" },
+  prim: {
+    str: "str",
+    num: 1,
+    bool: false,
+    null: null,
+    undef: undefined,
+  },
 };
 
 describe("getNested", () => {
   it("gets a simple root-level key", () => {
-    expect(getNested(testObj, "b")).toBe(testObj.b);
-  });
-  it("gets an object root-level key", () => {
-    expect(getNested(testObj, "c")).toEqual(testObj.c);
+    expect(getNested(testObj, "a")).toBe(testObj.a);
   });
   it("gets a simple nested key", () => {
-    expect(getNested(testObj, "c.d")).toBe(testObj.c.d);
+    expect(getNested(testObj, "b.c")).toBe(testObj.b.c);
+  });
+  it("gets an object root-level key", () => {
+    expect(getNested(testObj, "b")).toEqual(testObj.b);
+  });
+  it("gets all primitive types properly", () => {
+    Object.keys(testObj.prim).forEach((k) =>
+      // @ts-ignore
+      expect(getNested(testObj, `prim.${k}` as `prim.${keyof typeof testObj.prim}`)).toEqual(testObj.prim[k]));
   });
 });
