07c325e99b52936fd691ad17807c4414c4e4579d
Jiri Swen <jiri.ei@hotmail.com>
2021-09-21 17:17:15 +0200

    Remove optional access from getNested

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 exports.getNested = function getNested(record, key) {
   return key
     .split(".")
-    .reduce((r, k) => (typeof r === "object" ? r?.[k] : r), record);
+    .reduce((r, k) => (typeof r === "object" ? r[k] : r), record);
 }
