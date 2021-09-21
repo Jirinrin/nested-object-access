b5007de1d023b4aca09561cb199bcbedb68e5900
Jiri Swen <jiri.ei@hotmail.com>
2021-09-21 17:28:38 +0200

    Handle stupid stuff being passed as object into getNested

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 exports.getNested = function getNested(record, key) {
+  if (typeof record !== "object")
+    return undefined;
+
   return key
     .split(".")
     .reduce((r, k) => (typeof r === "object" ? r[k] : r), record);
